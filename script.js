fd320ce1ec2cf265fafb50e6ab822082bc064664
mmontesavilez <mmontesavilez28@brophybroncos.org>
2024-10-23 23:15:39 +0000

    done

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var factList = [
- "Fact 1",
- "Fact 2", 
- "Fact 3", 
- "Fact 4"];
+var factList = [ //makes a variable list in order
+ "Fact 1", //list term
+ "Fact 2", //list term
+ "Fact 3", //list term
+ "Fact 4"]; //list term
 
-var fact = document.getElementById("fact");
-var myBtn = document.getElementById("myBtn");
-var count = 0;
+var fact = document.getElementById("fact"); //creates variable fact
+var myBtn = document.getElementById("myBtn"); //creates variable myBtn
+var count = 0; //creates count variable 
 
-myBtn.addEventListener("click", displayFact);
+myBtn.addEventListener("click", displayFact); //when variable myBtn is clicked it will display fact
 
 function displayFact(){
-  fact.innerHTML = factList[count];
-  count++;
-  if (count == factList.length){
-    count = 0;
+  fact.innerHTML = factList[count]; //makes a function that works inside html using the factlist and count variable
+  count++; //add one to count
+  if (count == factList.length){ //if the count number equals the factlist length
+    count = 0; //then the count will be reset
   }
 }
